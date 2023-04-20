781e9e58539c941f0a1e527f8ba0df9d688375b6
Claverie Jean <jean@akigora.com>
2023-04-20 15:45:23 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home <PlusCircleOutline /></RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <div id="chart">
+    <apexchart height="350" width="50%" :options="chartOptions" :series="series"></apexchart>
+  </div>
+  <TheMostPopularTrackOfAArtist />
+</template>
+
+<script lang="ts">
+import { PlusCircleOutline } from 'mdue'
+import TheMostPopularTrackOfAArtist from './components/TheMostPopularTrackOfAArtist.vue'
+
+export default {
+  components: {
+    PlusCircleOutline,
+    TheMostPopularTrackOfAArtist
+  },
+  data: function () {
+    return {
+      chart: {
+        type: 'area'
+      },
+      stroke: {
+        show: true,
+        width: 4,
+        colors: ['#2f4cdd', '#b519ec']
+      },
+      series: [
+        {
+          name: 'series1',
+          data: [31, 40, 28, 51, 42, 109, 100]
+        },
+        {
+          name: 'series2',
+          data: [11, 32, 45, 32, 34, 52, 41]
+        },
+        {
+          name: 'series3',
+          data: [15, 32, 48, 3, 14, 32, 71]
+        }
+      ],
+      chartOptions: {
+        colors: ['#9DF3C4', '#62D2A2', '#1FAB89'],
+        chart: {
+          height: 350,
+          type: 'area',
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        title: {
+          text: '',
+          align: 'left'
+        },
+
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  align-self: center;
+  nav {
+    display: flex;
+    align-self: center;
+    column-gap: 15px;
+  }
+}
+</style>
---
+++ src/components/TheMostPopularTrackOfAArtist.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Top 10 des chansons d'Eminem sur Deezer</h1>
+    <ul>
+      <li v-for="(track, index) in tracks" :key="index">
+        {{ track.title }} - {{ track.album.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      tracks: []
+    }
+  },
+  mounted() {
+    this.getTopTracks()
+  },
+  methods: {
+    async getTopTracks() {
+      const res = await axios.get('https://api.deezer.com/artist/13/top?limit=5')
+      this.tracks = res.data.data
+    }
+  }
+}
+</script>
