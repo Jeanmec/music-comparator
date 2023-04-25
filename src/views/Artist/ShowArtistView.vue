7a0cbdbadf9fe55fafb59f9acce1707700d610ce
cdescamps <clementin.descamps@gmail.com>
2023-04-25 17:55:56 +0200

    .

--- src/views/Artist/ShowArtistView.vue
+++ src/views/Artist/ShowArtistView.vue
@@ -1,16 +1,37 @@
 <script>
-import './ShowArtist.style.scss'
+import ArtistPresentation from '../../components/Artist/ArtistPresentation.vue'
 import { getArtist } from '../../services/Artist/ArtistService.js'
-// import axios from 'axios'
+
 // import TheMostPopularTrackOfAArtist from '../../components/Artist/TheMostPopularTrackOfAArtist.vue'
-// import ArtistPresentation from '../../components/Artist/ArtistPresentation.vue'
 // import TreemapAlbumsFans from '../../components/Chart/TreemapAlbumsFans.vue'
 
 //todo if artist id is incorrect
 export default {
-  components: {},
+  components: { ArtistPresentation },
   data: () => {
-    return { artist: {} }
+    return {
+      artist: {
+        id: 13,
+        name: 'Eminem',
+        link: 'https:\/\/www.deezer.com\/artist\/13',
+        share:
+          'https:\/\/www.deezer.com\/artist\/13?utm_source=deezer&utm_content=artist-13&utm_term=0_1682437888&utm_medium=web',
+        picture: 'https:\/\/api.deezer.com\/artist\/13\/image',
+        picture_small:
+          'https:\/\/e-cdns-images.dzcdn.net\/images\/artist\/19cc38f9d69b352f718782e7a22f9c32\/56x56-000000-80-0-0.jpg',
+        picture_medium:
+          'https:\/\/e-cdns-images.dzcdn.net\/images\/artist\/19cc38f9d69b352f718782e7a22f9c32\/250x250-000000-80-0-0.jpg',
+        picture_big:
+          'https:\/\/e-cdns-images.dzcdn.net\/images\/artist\/19cc38f9d69b352f718782e7a22f9c32\/500x500-000000-80-0-0.jpg',
+        picture_xl:
+          'https:\/\/e-cdns-images.dzcdn.net\/images\/artist\/19cc38f9d69b352f718782e7a22f9c32\/1000x1000-000000-80-0-0.jpg',
+        nb_album: 58,
+        nb_fan: 16244967,
+        radio: true,
+        tracklist: 'https:\/\/api.deezer.com\/artist\/13\/top?limit=50',
+        type: 'artist'
+      }
+    }
   },
   methods: {},
 
@@ -49,12 +70,13 @@ export default {
 </script>
 
 <template>
-  <div>
-    <p>page artiste</p>
-    <div class="grid grid-cols-2 items-center">
-      <!-- <ArtistPresentation :artistData="artistData" /> -->
-      <!-- <TheMostPopularTrackOfAArtist :artistTopTracks="artistTopTracks" /> -->
-    </div>
+  <div class="flex flex-col my-5">
+    <ArtistPresentation :artist="artist" />
+
+    <!-- <div class="grid"> -->
+    <!-- <ArtistPresentation :artistData="artistData" /> -->
+    <!-- <TheMostPopularTrackOfAArtist :artistTopTracks="artistTopTracks" /> -->
+    <!-- </div> -->
     <!-- <TreemapAlbumsFans v-if="albumsData" :albumsData="albumsData" /> -->
   </div>
 </template>
